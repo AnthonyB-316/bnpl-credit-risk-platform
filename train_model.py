8581a8dbea9ba7eb4a090fe78f6957672bba6592
PlayfulAxe9 <71425700+PlayfulAxe9@users.noreply.github.com>
2025-12-08 13:31:15 -0500

    1
    Directories and initial scripts

---
+++ train_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from xgboost import XGBClassifier
+import pickle
+
+print("Loading dataset...")
+df = pd.read_csv('data/bnpl_sample_500.csv')
+
+X = df.drop('Default', axis=1)
+y = df['Default']
+
+print("Training XGBoost model...")
+model = XGBClassifier(
+    n_estimators=200,
+    max_depth=5,
+    learning_rate=0.1,
+    random_state=42,
+    eval_metric='logloss'
+)
+model.fit(X, y)
+
+print("Saving model to app/model.pkl...")
+with open('app/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved successfully!")
+print("You are now ready to deploy to AWS!")
